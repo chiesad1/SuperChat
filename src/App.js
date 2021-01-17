38a0a482bc6e61e62770c108c02e880686068fd9
chiesad1 <chiesad1@gmail.com>
2021-01-17 11:04:16 -0500

    initialized firebase dependencies and config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  //Config
+
+  apiKey: "AIzaSyB-Fzqy98NpNziu7UGoc3YQHWs6V8tIcRc",
+  authDomain: "chatdemo-d7cdd.firebaseapp.com",
+  databaseURL: "https://chatdemo-d7cdd.firebaseio.com",
+  projectId: "chatdemo-d7cdd",
+  storageBucket: "chatdemo-d7cdd.appspot.com",
+  messagingSenderId: "809550924762",
+  appId: "1:809550924762:web:f8eb99a292914b98560ee3",
+  measurementId: "G-0ST8F7SWGS"
+})
+
+const auth =firebase.auth;
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
